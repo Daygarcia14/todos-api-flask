0fb974b85fccfc57c89b8a3746dd941730676556
Daygarcia14 <dayanna_garcia@hotmail.com>
2022-04-28 12:05:59 +0000

    second commit

--- src/app.py
+++ src/app.py
@@ -1,14 +1,31 @@
-from crypt import methods
-from distutils.log import debug
-from flask import Flask
-
+from flask import Flask, jsonify, request
+import json
 app = Flask(__name__)
 
-todos = [{"label":"my first task for today", "done":False}, {"label": "go for dinner", "done":False}]
+todos = [
+    { "label": "My first task", "done": False },
+    { "label": "My second task", "done": False },
+    { "label": "My third task", "done": False },
+    { "label": "My fourth task", "done": False }
+]
 
-@app.route("/todos", methods=["GET"])
-def handle_todos():
+@app.route('/todos', methods=['GET'])
+def hello_world():
     return jsonify(todos)
 
-if __name__ == "__main__":
-    app.run(host="127.0.0.1", port=8000, debug=True)
+@app.route('/todos', methods=['POST'])
+def add_new_todo():
+    request_body = request.data
+    print("Incoming request with the following body", request_body)
+    decoded_object = json.loads(request_body)
+    todos.append(decoded_object)
+    return jsonify(todos)
+
+@app.route('/todos/<int:position>', methods=['DELETE'])
+def delete_todo(position):
+    print("This is the position to delete: ", position)
+    todos.pop(position)
+    return jsonify(todos)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=3245, debug=True)
